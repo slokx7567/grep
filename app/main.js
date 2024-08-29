50875c6b1090d1e3a72a21b0c7594bd8ba8d983b
slokx7567 <princebhayani127@gmail.com>
2024-08-30 00:27:35 +0530

    run tests

--- app/main.js
+++ app/main.js
@@ -1,19 +1,10 @@
 function matchPattern(inputLine, pattern) {
+  let regex = pattern.replace('\\d', '[0-9]').replace('\\w', '[0-9a-zA-Z]');
+
   if (pattern.length === 1) {
     return inputLine.includes(pattern);
-  }else if(pattern === '\\d'){
-    return /\d/.test(inputLine);
-  }else if( pattern === '\\w'){
-    return /\w/.test(inputLine);
-  }else if(pattern.startsWith('[') && pattern.endsWith(']')) {
-    const ptr = new RegExp(pattern)
-    return ptr.test(inputLine);
-  }else if(!(pattern.startsWith('[') && pattern.endsWith(']'))) {
-    const ptr = new RegExp(pattern)
-    return ptr.test(inputLine);
-  }
-  else {
-    throw new Error(`Unhandled pattern ${pattern}`);
+  } else {
+    return new RegExp(regex).test(inputLine);
   }
 }
 
