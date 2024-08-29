59ab30df0b2e7dff3387360c2f6084dc415f5e14
slokx7567 <princebhayani127@gmail.com>
2024-08-29 22:46:09 +0530

    pass 1st stage

--- app/main.js
+++ app/main.js
@@ -15,15 +15,12 @@ function main() {
     process.exit(1);
   }
 
-  // You can use print statements as follows for debugging, they'll be visible when running tests.
   console.log("Logs from your program will appear here");
-
-  // Uncomment this block to pass the first stage
-  // if (matchPattern(inputLine, pattern)) {
-  //   process.exit(0);
-  // } else {
-  //   process.exit(1);
-  // }
+  if (matchPattern(inputLine, pattern)) {
+    process.exit(0);
+  } else {
+    process.exit(1);
+  }
 }
 
 main();
