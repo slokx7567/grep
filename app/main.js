1e43b61994e6b5bc0d24ff78fd3afa7616f2ef3a
slokx7567 <princebhayani127@gmail.com>
2024-08-30 02:53:51 +0530

    run tests

--- app/main.js
+++ app/main.js
@@ -5,7 +5,7 @@ function matchPattern(inputLine, pattern) {
     pattern = pattern.slice(1, pattern.length)
   }else if(pattern[inputLine.length-1] === '$'){
     pattern = pattern.slice(0, pattern.length-1)
-  }else if(pattern.includes('+')){
+  }else if(pattern.includes('+') || pattern.includes('?')){
     const regExp = new RegExp(pattern);
 
     return regExp.test(inputLine);
